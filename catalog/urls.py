ea9f963402fecfb98450b26b0c89d6d66a160000
Jasser Tayari <jassertayari101@gmail.com>
2022-11-20 21:19:41 +0100

    added the ability to view all authors and their details,all books and their details

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index,name='index')
+    path('',views.index,name='index'),
+    path('books',views.list_books,name='list_books'),
+    path('book/<int:book_id>',views.book_detail,name='book-detail'),
+    path('authors',views.list_authors,name='list_authors'),
+    path('author/<int:author_id>',views.author_detail,name='author-detail')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Book,BookInstance,Author
-# Create your views here.
+from django.core.paginator import Paginator,PageNotAnInteger,EmptyPage
+
 
 def index(request):
     counts = [
@@ -10,3 +11,51 @@ def index(request):
         {'name':'Authors','count':(Author.objects.all()).count()},
     ]
     return render(request,'catalog/index.html',{'counts':counts})
+
+
+def list_books(request):
+    all_books = Book.objects.all()
+    #
+    #paginator = Paginator(all_books,4)
+    #
+    #page = request.GET.get('page')
+   #
+    #try:
+    #    books = paginator(page)
+    #except PageNotAnInteger:
+    #    books = paginator(1)
+    #except EmptyPage:
+    #    books = paginator(paginator.num_pages)
+    #
+    return render(request,'catalog/books.html',{'books':all_books})
+
+def book_detail(request,book_id):
+    book = get_object_or_404(Book,id=book_id)
+    copies = BookInstance.objects.filter(book_id=book_id)
+    
+    return render(request,f'catalog/book_detail.html',{'book':book,'copies':copies})
+
+
+def list_authors(request):
+    all_authors = Author.objects.all()
+    
+    #paginator = Paginator(all_authors,4)
+   
+    #page = request.GET.get('page')
+   
+    #try:
+    #    authors = paginator(page)
+    #except PageNotAnInteger:
+    #    authors = paginator(1)
+    #except EmptyPage:
+    #    authors = paginator(paginator.num_pages)
+    
+    return render(request,'catalog/authors.html',{'authors':all_authors})
+
+def author_detail(request,author_id):
+    author = get_object_or_404(Author,id=author_id)
+    
+    if author: 
+        books = Book.objects.filter(author_id=author_id)  
+    
+    return render(request,f'catalog/author_detail.html',{'author':author,'books':books})
